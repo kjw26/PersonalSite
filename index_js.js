7cc43d0e83e6dfdcee1209fa300039f68d73d425
Khadir Williams <kjw26@njit.edu>
2018-03-01 22:51:46 -0500

    Updated Index Page

--- index_js.js
+++ index_js.js
@@ -1,43 +1,49 @@
-<<<<<<< HEAD:index_js.js
-// Change style of navbar on scroll
-window.onscroll = function () { myFunction() };
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white" ;
-    } else {
-        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white","");
-    }
-}
+	
+	var bottom = $('#introDiv').offset().top + $('#introDiv').height();
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-=======
-// Change style of navbar on scroll
-window.onscroll = function () { myFunction() };
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white" ;
-    } else {
-        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white","");
-    }
-}
+	$(window).on('scroll',function(){
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > bottom) {
+        $('.navbar').addClass('scrolled');
     } else {
-        x.className = x.className.replace(" w3-show", "");
+        $('.navbar').removeClass('scrolled');
     }
-}
->>>>>>> fbcd24a5a8a584e5f269ad829b7c4daab5d528d6:index_js.js
+
+	});
+	
+	/*$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})*/
